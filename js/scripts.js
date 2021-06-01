4492c0927bed345e3d04f5122c911273ecc529b0
William Ma <williamma2020@icloud.com>
2021-06-01 10:43:31 -0700

    style jumbotron

--- js/scripts.js
+++ js/scripts.js
@@ -54,20 +54,4 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 
-    // Magnific popup calls (changed #portfolio to #portfolio-image, as I need put another a link in card)
-    // $('.portfolio-image').magnificPopup({
-    //     delegate: 'a',
-    //     type: 'image',
-    //     tLoading: 'Loading image #%curr%...',
-    //     mainClass: 'mfp-img-mobile',
-    //     gallery: {
-    //         enabled: true,
-    //         navigateByImgClick: true,
-    //         preload: [0, 1]
-    //     },
-    //     image: {
-    //         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    //     }
-    // });
-
 })(jQuery); // End of use strict
